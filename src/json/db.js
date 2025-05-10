34659ace4a7f87ed1f9b0d1e9dc9d25d313c7667
mukeshh24 <s.mukesh1403@gmail.com>
2025-05-10 16:04:01 +0530

    code push again add all files

---
+++ src/json/db.js
@@ -0,0 +1,274 @@
+export const desktopIcon = [
+  {
+    id: 1,
+    icon: "./close-icon.png",
+    bgColor: "#f96057",
+  },
+  {
+    id: 2,
+    icon: "./subtract-icon.png",
+    bgColor: "#f8ce52",
+  },
+  {
+    id: 3,
+    icon: "./full-screen-icon.png",
+    bgColor: "#5fcf65",
+  },
+];
+
+export const navLinks = [
+  {
+    id: 1,
+    title: "About",
+    link: "#about",
+    icon: "./flag.png",
+  },
+  {
+    id: 2,
+    title: "Skills",
+    link: "#skills",
+    icon: "./angular.png",
+  },
+  {
+    id: 3,
+    title: "Jobs",
+    link: "#jobs",
+    icon: "./work.png",
+  },
+  {
+    id: 4,
+    title: "Projects",
+    link: "#projects",
+    icon: "./code.png",
+  },
+  {
+    id: 5,
+    title: "Educations",
+    link: "#educations",
+    icon: "./university.png",
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    title: "LinkedIn",
+    link: "/linkedIn",
+    icon: "./linkedin.png",
+  },
+  {
+    id: 2,
+    title: "Github",
+    link: "/github",
+    icon: "./github.png",
+  },
+  {
+    id: 3,
+    title: "Phone",
+    link: "/phone",
+    icon: "./phone.png",
+  },
+  {
+    id: 4,
+    title: "Email",
+    link: "/email",
+    icon: "./email.png",
+  },
+];
+
+export const skillsData = [
+  {
+    id: 1,
+    title: "Html",
+    icon: "./html.png"
+  },
+  {
+    id: 2,
+    title: "CSS",
+    icon: "./css.png"
+  },
+  {
+    id: 3,
+    title: "SCSS",
+    icon: "./scss.png"
+  },
+  {
+    id: 4,
+    title: "JavaScript",
+    icon: "./javascript.png"
+  },
+  {
+    id: 6,
+    title: "JQuery",
+    icon: "./jquery.png"
+  },
+  {
+    id: 7,
+    title: "ReactJs",
+    icon: "./reactjs.png"
+  },
+  {
+    id: 8,
+    title: "Tailwind",
+    icon: "./tailwind.png"
+  },
+  {
+    id: 9,
+    title: "Bootstrap",
+    icon: "./bootstrap.png"
+  },
+  {
+    id: 10,
+    title: "Git",
+    icon: "./git.png"
+  },
+];
+
+export const jobsData = [
+  {
+    id: 1,
+    title: "KARN Marketing Warfare LLP",
+    companyLogo: "./karn-marketing.png",
+    joiningDate: "Apr 2024 - Present",
+    companyOneLIne: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`,
+    companyPoints: [
+      {
+        id: 1,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 2,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 3,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 4,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: "Webiknows IT Solutions",
+    companyLogo: "./webiknows-it-solutions.png",
+    joiningDate: "Apr 2023 - Apr 2024 · 1 yr 1 mo",
+    companyOneLIne: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`,
+    companyPoints: [
+      {
+        id: 1,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 2,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 3,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 4,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: "Cloudstrats",
+    companyLogo: "./cloudstrats.png",
+    joiningDate: "Apr 2023 - Apr 2024 · 1 yr 1 mo",
+    companyOneLIne: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`,
+    companyPoints: [
+      {
+        id: 1,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 2,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 3,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+      {
+        id: 4,
+        title: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A id ipsa illum architecto quidem impedit neque magnam
+                        illo ducimus consectetur.`
+      },
+    ]
+  },
+];
+
+export const projectData = [
+  {
+    id: 1,
+    pTitle: "hello worl how hurj hvh",
+    pDesc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Provident vitae minima culpa officiis eius obcaecati odio,
+                      rerum ipsum id eum incidunt! Optio laborum illo quisquam
+                      tempora placeat aut consequuntur nisi, maiores, dolor
+                      autem vel aliquam recusandae sint quae eligendi maxime?
+                      Dignissimos quod libero, ex voluptas nostrum corrupti
+                      eligendi explicabo voluptatum atque iste tempora quisquam
+                      voluptatibus, optio unde, consequuntur beatae! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Provident vitae minima culpa officiis eius obcaecati odio,
+                      rerum ipsum id eum incidunt! Optio laborum illo quisquam
+                      tempora placeat aut consequuntur nisi, maiores, dolor
+                      autem vel aliquam recusandae sint quae eligendi maxime?
+                      Dignissimos quod libero, ex voluptas nostrum corrupti
+                      eligendi explicabo voluptatum atque iste tempora quisquam
+                      voluptatibus, optio unde, consequuntur beatae!`
+  },
+  {
+    id: 2,
+    pTitle: "hello worl how hurj hvh",
+    pDesc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Provident vitae minima culpa officiis eius obcaecati odio,
+                      rerum ipsum id eum incidunt! Optio laborum illo quisquam
+                      tempora placeat aut consequuntur nisi, maiores, dolor
+                      autem vel aliquam recusandae sint quae eligendi maxime?
+                      Dignissimos quod libero, ex voluptas nostrum corrupti
+                      eligendi explicabo voluptatum atque iste tempora quisquam
+                      voluptatibus, optio unde, consequuntur beatae! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Provident vitae minima culpa officiis eius obcaecati odio,
+                      rerum ipsum id eum incidunt! Optio laborum illo quisquam
+                      tempora placeat aut consequuntur nisi, maiores, dolor
+                      autem vel aliquam recusandae sint quae eligendi maxime?
+                      Dignissimos quod libero, ex voluptas nostrum corrupti
+                      eligendi explicabo voluptatum atque iste tempora quisquam
+                      voluptatibus, optio unde, consequuntur beatae!`
+  },
+]
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import "./home.css";
+import {
+  desktopIcon,
+  jobsData,
+  navLinks,
+  projectData,
+  skillsData,
+  socialLinks,
+} from "../../json/db";
+
+const Home = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const toggleFullScreen = () => {
+    if (!isFullScreen) {
+      document.documentElement
+        .requestFullscreen()
+        .then(() => {
+          setIsFullScreen(true);
+        })
+        .catch((err) => {
+          console.error("Error attempting to enable full-screen mode:", err);
+        });
+    } else {
+      document
+        .exitFullscreen()
+        .then(() => {
+          setIsFullScreen(false);
+        })
+        .catch((err) => {
+          console.error("Error attempting to exit full-screen mode:", err);
+        });
+    }
+  };
+  return (
+    <div className="home-page">
+      <div className="main-background">
+        <video width={320} height={240} loop autoPlay muted>
+          <source src="./background-video.mp4" />
+        </video>
+      </div>
+      <div className="personal-main-container">
+        <div className="personal-container-row">
+          <div className="left-side">
+            <div className="left-side-header">
+              {desktopIcon.map((ele) => {
+                return (
+                  <div
+                    key={ele.id}
+                    className="button-block"
+                    style={{ backgroundColor: ele.bgColor }}
+                    onClick={toggleFullScreen}
+                  >
+                    <img src={ele.icon} alt={ele.icon} />
+                  </div>
+                );
+              })}
+            </div>
+            <div className="left-side-body hide-scrollbar">
+              <div className="left-side-body-block">
+                <p className="left-side-body-block-paragraph">Categories</p>
+                <nav>
+                  {navLinks.map((ele) => {
+                    return (
+                      <a
+                        href={ele.link}
+                        key={ele.id}
+                        className="left-side-body-block-link"
+                      >
+                        <img src={ele.icon} alt={ele.title} />
+                        <p>{ele.title}</p>
+                      </a>
+                    );
+                  })}
+                </nav>
+              </div>
+              <div className="left-side-body-block">
+                <p className="left-side-body-block-paragraph">Social Links</p>
+                <nav>
+                  {socialLinks.map((ele) => {
+                    return (
+                      <a
+                        href={ele.title}
+                        key={ele.id}
+                        className="left-side-body-block-link"
+                      >
+                        <img src={ele.icon} alt={ele.title} />
+                        <p>{ele.title}</p>
+                      </a>
+                    );
+                  })}
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div className="right-side">
+            <div className="right-side-header">
+              <img src="./avtar.jpg" alt="mukesh suthar avtar image" />
+            </div>
+            <div className="right-side-body hide-scrollbar">
+              <div id="about" className="about-block">
+                <div className="about-info-block">
+                  <h1>Mukesh Suthar</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+                    eligendi, adipisci autem cupiditate mollitia, accusamus
+                    quasi, nobis obcaecati iste architecto non deserunt eius
+                    modi illo cumque similique recusandae ea neque. Unde sunt,
+                    modi, doloribus error corrupti, delectus itaque impedit ad
+                    sequi at laudantium. Amet sapiente eos corrupti unde,
+                    mollitia fuga facere porro aut aperiam at. Nemo vitae
+                    facilis consequuntur cumque illum odio ad tempore vero, eos
+                    omnis, rem consequatur nisi architecto accusantium
+                    perspiciatis optio aliquid animi nesciunt. Tenetur sint
+                    consequatur hic, voluptas harum voluptatem fuga optio qui
+                    itaque natus delectus sit? Consequatur magnam similique
+                    facere autem earum enim aut aperiam.
+                  </p>
+                  <button to={"/"} className="about-download-btn">
+                    Download Resume
+                  </button>
+                </div>
+                <img
+                  src="./front-end-developer.png"
+                  className="about-block-img"
+                  alt=""
+                />
+              </div>
+              <div id="skills" className="skills-block">
+                <h4>My Skills</h4>
+                <div className="skills-block-row">
+                  {skillsData.map((ele) => {
+                    return (
+                      <div key={ele.id} className="skills-content">
+                        <img src={ele.icon} alt={ele.title} />
+                        <p>{ele.title}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div id="jobs" className="jobs-block">
+                <h4>My Jobs</h4>
+                <div className="jobs-block-row">
+                  {jobsData.map((ele) => {
+                    return (
+                      <div key={ele.id} className="jobs-content">
+                        <div className="jobs-content-header">
+                          <div className="jobs-content-header-left">
+                            <img src={ele.companyLogo} alt={ele.title} />
+                            <p>{ele.title}</p>
+                          </div>
+                          <div className="jobs-content-header-right">
+                            <p>{ele.joiningDate}</p>
+                          </div>
+                        </div>
+                        <div className="jobs-content-body">
+                          <p>{ele.companyOneLIne}</p>
+                          <ul>
+                            {ele.companyPoints.map((point) => {
+                              return <li key={point.id}>{point.title}</li>;
+                            })}
+                          </ul>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div id="projects" className="projects-block">
+                <h4>My Projects</h4>
+                <div className="projects-block-row">
+                  {projectData.map((ele) => {
+                    return (
+                      <div key={ele.id} className="projects-content">
+                        <h5>{ele.pTitle}</h5>
+                        <p>{ele.pDesc}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div id="educations" className="education-block">
+                <h4>My Education</h4>
+                <div className="education-block-row">
+                  <div className="education-content">
+                    <div className="education-content-left-block">
+                      <img src="./bvu.png" alt="" />
+                      <div>
+                        <h5>Bachelor of Computer Application</h5>
+                        <h6>Bharati Vidyapeeth University</h6>
+                      </div>
+                    </div>
+                    <div className="education-content-right-block">
+                      <p>2022 Oct – present</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
